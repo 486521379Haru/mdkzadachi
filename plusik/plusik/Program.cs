8763cc2966000243c4fad1d7665f95ed0e4046bf
Haru_Marden <133318413+486521379Haru@users.noreply.github.com>
2024-09-13 13:48:22 +0400

    Add files via upload

---
+++ plusik/plusik/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Простой калькулятор для сложения двух чисел");
+
+        // Запрос первого числа
+        Console.Write("ввести A: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        // Запрос второго числа
+        Console.Write("ввести B: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        // Вычисление суммы
+        double result = AddNumbers(number1, number2);
+
+        // Вывод результата
+        Console.WriteLine($"Результат: {number1} + {number2} = {result}");
+    }
+
+    static double AddNumbers(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+}
